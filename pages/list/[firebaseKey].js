9713a699d1d48929a89eab51735a4c49d95724d5
Jasper Baltz <nickolas.baltz@gmail.com>
2023-05-08 19:07:03 -0500

    added pages-lists folder files. can be edited later.

---
+++ pages/list/[firebaseKey].js
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { viewListDetails } from '../../api/mergedData';
+
+export default function ViewList() {
+  const [listDetails, setListDetails] = useState({});
+  const router = useRouter();
+
+  const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    viewListDetails(firebaseKey).then(setListDetails);
+  }, [firebaseKey]);
+
+  return (
+    <>
+      <h1>{listDetails.name}</h1>
+    </>
+  );
+}
---
+++ pages/list/edit/[firebasKey].js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { getSingleList } from '../../../api/listData';
+import ListForm from '../../../components/forms/ListForm';
+
+export default function EditList() {
+  const [editItem, setEditItem] = useState({});
+  const router = useRouter();
+
+  const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    getSingleList(firebaseKey).then(setEditItem);
+  }, [firebaseKey]);
+
+  return (<ListForm obj={editItem} />);
+}
